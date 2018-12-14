afaedd3489638c5d972e795de6bf5da5e6c2918d
Daniel Gittings <dangittings@gmail.com>
2018-12-14 20:03:24 +0000

    Decrease length of sentences

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -77,7 +77,7 @@ class Generator extends Component {
 
       // Get a random number that defines the number of words per sentence
       // For this app we define a sentence as being between 8 and 15 words.
-      const numWords = this.getRandomNumber(4, 8);
+      const numWords = this.getRandomNumber(4, 6);
 
       for (let i = 1; i <= numWords; i++) {
         let rand = this.getRandomNumber(0, length + 1);
